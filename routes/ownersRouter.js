dc0ea1f2b3a25432976b112ae54ac5e696788d77
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-10 17:08:07 +0545

    some error to fix in env

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -12,9 +12,18 @@ console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV==="development"){
    router.post('/create',async function(req,res){
     let owners=await ownerModel.find();
-    if(owners.length>0) return res
+    if(owners.length>0){
+    return res
     .send(503)
-    .send("you dont have permission");
+    .send("you dont have permission");}
+    
+    let {fullname,email,password}=req.body;
+    let createdowner=await ownerModel.create({
+    fullname,
+    email,
+    password,
+    });
+    res.status(201).send(createdowner);
 })
     
 }
