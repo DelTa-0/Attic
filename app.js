c346f1b2c2038428c2c234e081505160e5cffd14
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-17 22:49:54 +0545

    index file

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 app.set('view engine','ejs');
 
+
 app.use("/owners",ownersRouter);
 app.use("/users",usersRouter);
 app.use("/products",productsRouter);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router=express.Router();
+const userModel = require('../models/user-model');
 
 router.get('/',function(req,res){
     res.send("hey");
+})
+
+router.get('/register',(req,res)=>{
+res.render('index', { error: 'error occured' }); 
+
+})
+
+router.post('/register',async function(req,res){
+    try{
+        let {email,username,fullname}=req.body;
+    let user=await userModel.create({
+        email,
+        password,
+        fullname,
+
+    });
+    res.send(user);
+    }
+    catch(err){
+        console.log(err.message);
+    }
 })
 
 module.exports=router;
