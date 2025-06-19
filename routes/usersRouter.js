8d1e7e2a52ea8305e577a96630d74ff5b7ea4f72
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-19 15:27:49 +0545

    userRegister controller done

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,30 +1,11 @@
 const express = require('express');
 const router=express.Router();
-const userModel = require('../models/user-model');
+const {registerUser} = require('../controllers/authController');
 
 router.get('/',function(req,res){
-    res.send("hey");
+    res.send("hey working");
 })
 
-router.get('/register',(req,res)=>{
-res.render('index', { error: 'error occured' }); 
-
-})
-
-router.post('/register',async function(req,res){
-    try{
-        let {email,username,fullname}=req.body;
-    let user=await userModel.create({
-        email,
-        password,
-        fullname,
-
-    });
-    res.send(user);
-    }
-    catch(err){
-        console.log(err.message);
-    }
-})
+router.post('/register',registerUser);
 
 module.exports=router;
