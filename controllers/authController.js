2192bfb3fba3882b0c95869ecb28eda94da44042
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-24 19:18:12 +0545

    creating product

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,7 +42,7 @@ module.exports.loginUser=async function(req,res){
         if(result){
             let token=generateToken(user);
             res.cookie("token",token);
-            res.send("you can login");
+            res.redirect("/shop");
         }
         else{
             return res.send("email or password incorrect");
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,24 +2,45 @@ const express = require('express');
 const router=express.Router();
 const ownerModel = require('../models/owner-model');
 const productModel = require('../models/product-model');
-if(process.env.NODE_ENV==="development"){
-   router.post('/create',async function(req,res){
-    let owners=await ownerModel.find();
-    if(owners.length>0){
-    return res.send(503).send("you dont have permission");}
+const upload = require('../config/multer-config');
+
+// if(process.env.NODE_ENV==="development"){
+//    router.post('/create',async function(req,res){
+//     let owners=await ownerModel.find();
+//     if(owners.length>0){
+//     return res.send(503).send("you dont have permission");}
     
-    let {image,name,price}=req.body;
-    let createdProduct=await productModel.create({
-    image,
-    name,
-    price,
+//     let {image,name,price,discount,bgcolor,panelcolor,textcolor}=req.body;
+//     let createdProduct=await productModel.create({
+//     image,
+//     name,
+//     price,
+//     discount,
+//     bgcolor,
+//     panelcolor,
+//     textcolor
+//     });
+//     res.status(201).send(createdProduct);
+// })
+    
+// }
+router.post('/create',upload.single("image"),async function(req,res){
+    try{
+        let {name,price,discount,bgcolor,panelcolor,textcolor}=req.body;
+    let product=await productModel.create({
+        image:req.file.buffer,
+            name,
+            price,
+            discount,
+            bgcolor,
+            panelcolor,
+            textcolor
     });
-    res.status(201).send(createdProduct);
-})
-    
-}
-router.get('/',function(req,res){
-    res.send("hey");
+    req.flash("success","product created successfully");
+    res.redirect("/owners/admin");
+    }catch(err){
+        res.send(err.message);
+    }
 })
 
 module.exports=router;
