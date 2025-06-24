c3b570052a379826649591514671634274fce7e4
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-24 18:33:14 +0545

    logout added

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router=express.Router();
-
+const ownerModel = require('../models/owner-model');
+const productModel = require('../models/product-model');
+if(process.env.NODE_ENV==="development"){
+   router.post('/create',async function(req,res){
+    let owners=await ownerModel.find();
+    if(owners.length>0){
+    return res.send(503).send("you dont have permission");}
+    
+    let {image,name,price}=req.body;
+    let createdProduct=await productModel.create({
+    image,
+    name,
+    price,
+    });
+    res.status(201).send(createdProduct);
+})
+    
+}
 router.get('/',function(req,res){
     res.send("hey");
 })
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router=express.Router();
 const {registerUser} = require('../controllers/authController');
 const {loginUser} = require('../controllers/authController');
+const {logout}=require("../controllers/authController");
 
 router.get('/',function(req,res){
     res.send("hey working");
@@ -10,5 +11,8 @@ router.get('/',function(req,res){
 router.post('/register',registerUser);
 
 router.post("/login",loginUser);
+
+
+router.get("/logout",logout);
 
 module.exports=router;
